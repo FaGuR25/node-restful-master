c2003ebd73d31c06b3cf39946cbec2b9dccb793c
Fatima <guadalupe20050@gmail.com>
2024-05-24 13:15:08 -0600

    cambios 24 mayo

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -3,13 +3,14 @@ const knex = require('knex');
 
 async function connectMysql() {
     const mysqlPassword = await fs.promises.readFile('/run/secrets/mysql_password');
+    console.log("mysqlPassword "+mysqlPassword);
     const db = knex({
         client: 'mysql2',
         connection: {
             host: 'mysql', // docker-compose service
             user: 'root',
             password: mysqlPassword,
-            database: 'enfermedad'
+            database: 'enfermedades'
         }
     });
     return db;
